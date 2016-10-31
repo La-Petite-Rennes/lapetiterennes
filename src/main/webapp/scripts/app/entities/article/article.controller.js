57fcc992dcc67aa96bce3976d9e872a40502d27b
glebreton <goulven.lebreton@niji.fr>
2016-10-31 11:25:22 +0100

    [Article] Ajout tri + code couleur en fonction de la quantité

--- src/main/webapp/scripts/app/entities/article/article.controller.js
+++ src/main/webapp/scripts/app/entities/article/article.controller.js
@@ -3,12 +3,29 @@
 angular.module('membershipApp')
 	.controller('ArticleController', function ($scope, Article) {
 		$scope.articles = [];
+		$scope.reverse = false;
+		$scope.propertyName= 'name';
 		
 		$scope.loadAll = function() {
 			Article.query(function(result) {
 				$scope.articles = result;
 			})
 		}
+		
+		$scope.stockLevel = function(article) {
+			if (article.quantity === 0) {
+				return 'article-outOfStock';
+			} else if (article.quantity <= 5) {
+				return 'article-insufficientStock';
+			} else {
+				return 'article-sufficientStock';
+			}
+		}
+		
+		$scope.sortBy = function(propertyName) {
+			$scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+			$scope.propertyName = propertyName;
+		};
 		
 		$scope.loadAll();
 		
