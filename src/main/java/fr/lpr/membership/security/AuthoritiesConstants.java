4622e01357fd93dc7c035e94befcf4ebf516545c
glebreton <goulven.lebreton@niji.fr>
2016-11-15 13:59:55 +0100

    Ajout du rôle "Responsable d'atelier"

--- src/main/java/fr/lpr/membership/security/AuthoritiesConstants.java
+++ src/main/java/fr/lpr/membership/security/AuthoritiesConstants.java
@@ -9,7 +9,9 @@ public final class AuthoritiesConstants {
     }
 
     public static final String ADMIN = "ROLE_ADMIN";
-
+    
+    public static final String WORKSHOP_MANAGER = "ROLE_WORKSHOP_MANAGER";
+    
     public static final String USER = "ROLE_USER";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
