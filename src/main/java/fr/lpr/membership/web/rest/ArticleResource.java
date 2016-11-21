b554d3a18c16095f1c45cbee3d1ca178a7d78bc5
glebreton <goulven.lebreton@niji.fr>
2016-11-21 13:59:59 +0100

    Refactoring gestion des articles suite à ajout entités en BDD

--- src/main/java/fr/lpr/membership/web/rest/ArticleResource.java
+++ src/main/java/fr/lpr/membership/web/rest/ArticleResource.java
@@ -1,14 +1,19 @@
 package fr.lpr.membership.web.rest;
 
+import static com.google.common.collect.ImmutableList.of;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,19 +26,36 @@ import com.codahale.metrics.annotation.Timed;
 
 import fr.lpr.membership.domain.Article;
 import fr.lpr.membership.domain.Provider;
+import fr.lpr.membership.domain.stock.Reassort;
+import fr.lpr.membership.repository.ArticleRepository;
+import fr.lpr.membership.repository.ProviderRepository;
+import fr.lpr.membership.service.stock.ReassortService;
 
 @RestController
 @RequestMapping("/api")
 public class ArticleResource {
 
-	private List<Article> articles;
-	
+	@Autowired
+	private ArticleRepository articleRepository;
+
+	@Autowired
+	private ProviderRepository providerRepository;
+
+	@Autowired
+	private ReassortService reassortService;
+
+	// FIXME A Supprimer
 	@PostConstruct
 	public void init() {
+		if (!providerRepository.findAll().isEmpty()) {
+			return;
+		}
+
 		Provider p1 = new Provider().name("Chain Reaction Cycles");
 		Provider p2 = new Provider().name("Probike Shop");
-		
-		articles = new ArrayList<>(5);
+		providerRepository.save(of(p1, p2));
+
+		List<Article> articles = new ArrayList<>(5);
 		articles.add(new Article().name("Dérailleur arrière Shimano Tiagra 4700 10v")
 				.id(13L).salePrice(3199).quantity(15).provider(p1).unitPrice(2599));
 		articles.add(new Article().name("Cassette Route Shimano Ultegra 6800 11 vitesses")
@@ -41,29 +63,35 @@ public class ArticleResource {
 		articles.add(new Article().id(5L).name("Frein Shimano Dura-Ace 9000").salePrice(11799).quantity(3).provider(p2).unitPrice(11000));
 		articles.add(new Article().id(8L).name("Pneu Route Continental Grand Prix 4000S II - 23c PAIR").salePrice(6999).provider(p2).unitPrice(5150).quantity(25));
 		articles.add(new Article().id(15L).name("Cable de frein").quantity(47));
+		articleRepository.save(articles);
 	}
-	
+
 	@RequestMapping(value="/articles", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	@Timed
 	public List<Article> getAll() {
-		return articles;
+		return articleRepository.findAll();
 	}
-	
+
 	@RequestMapping(value="/articles/{id}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	@Timed
 	public ResponseEntity<Article> get(@PathVariable Long id) {
-		if (id < 0 || id > articles.size()) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		} else {
-			return new ResponseEntity<>(articles.get(id.intValue()), HttpStatus.OK);
-		}
+		return Optional.ofNullable(articleRepository.findOne(id))
+				.map(a -> new ResponseEntity<>(a, OK))
+				.orElse(new ResponseEntity<>(NOT_FOUND));
 	}
-	
+
 	@RequestMapping(value = "/articles", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	@Timed
 	public ResponseEntity<Void> create(@Valid @RequestBody Article article) throws URISyntaxException {
-		this.articles.add(article);
-		return ResponseEntity.created(new URI("/api/articles/" + (articles.size()-1))).build();
+		Article savedArticle = articleRepository.save(article);
+		return ResponseEntity.created(new URI("/api/articles/" + savedArticle.getId())).build();
+	}
+
+	@RequestMapping(value="/articles/reassort", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+	@Timed
+	public ResponseEntity<Void> reassort(List<Reassort> reassorts) {
+		reassortService.reassort(reassorts);
+		return ResponseEntity.ok().build();
 	}
 
 }
