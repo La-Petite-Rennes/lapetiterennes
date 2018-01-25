6b00f780e20b18ac3a628d3255671e8f3532907d
Goulven Le Breton <goulven.lebreton@gmail.com>
2018-01-25 13:24:51 +0100

    Refactoring

--- src/main/java/fr/lpr/membership/service/ExportService.java
+++ src/main/java/fr/lpr/membership/service/ExportService.java
@@ -42,45 +42,41 @@ import java.util.stream.Collectors;
 @Service
 public class ExportService {
 
-	public static final String CSV = "csv";
-	public static final String XML = "xml";
-	public static final String JSON = "json";
+    public static final String CSV = "csv";
+    public static final String XML = "xml";
+    public static final String JSON = "json";
 
-	private static final String CONTENT_TYPE_HEADER = "Content-Type";
+    private static final String CONTENT_TYPE_HEADER = "Content-Type";
 
-	private static final Map<String, BiConsumer<Adherent, AdherentDto>> DTO_MAPPER = ImmutableMap.<String, BiConsumer<Adherent, AdherentDto>> builder()
-			.put("id", (a, dto) -> dto.id = a.getId()).put("nom", (a, dto) -> dto.nom = a.getNom()).put("prenom", (a, dto) -> dto.prenom = a.getPrenom())
-			.put("estBenevole", (a, dto) -> dto.estBenevole = a.getBenevole())
-			.put("adresse", (a, dto) -> dto.adresse = a.getCoordonnees().getAdresseComplete())
-			.put("codePostal", (a, dto) -> dto.codePostal = a.getCoordonnees().getCodePostal())
-			.put("ville", (a, dto) -> dto.ville = a.getCoordonnees().getVille()).put("email", (a, dto) -> dto.email = a.getCoordonnees().getEmail())
-			.put("telephone", (a, dto) -> dto.telephone = a.getCoordonnees().getTelephone()).put("adhesions", (a, dto) -> {
-				dto.adhesions = a.getAdhesions().stream().map(ad -> new AdhesionDto(ad.getTypeAdhesion(), ad.getDateAdhesion())).collect(Collectors.toList());
-				dto.lastAdhesion = a.getLastAdhesion();
-			}).build();
+    private static final Map<String, BiConsumer<Adherent, AdherentDto>> DTO_MAPPER = ImmutableMap.<String, BiConsumer<Adherent, AdherentDto>>builder()
+        .put("id", (a, dto) -> dto.id = a.getId()).put("nom", (a, dto) -> dto.nom = a.getNom()).put("prenom", (a, dto) -> dto.prenom = a.getPrenom())
+        .put("estBenevole", (a, dto) -> dto.estBenevole = a.getBenevole())
+        .put("adresse", (a, dto) -> dto.adresse = a.getCoordonnees().getAdresseComplete())
+        .put("codePostal", (a, dto) -> dto.codePostal = a.getCoordonnees().getCodePostal())
+        .put("ville", (a, dto) -> dto.ville = a.getCoordonnees().getVille())
+        .put("email", (a, dto) -> dto.email = a.getCoordonnees().getEmail())
+        .put("telephone", (a, dto) -> dto.telephone = a.getCoordonnees().getTelephone()).put("adhesions", (a, dto) -> {
+            dto.adhesions = a.getAdhesions().stream().map(ad -> new AdhesionDto(ad.getTypeAdhesion(), ad.getDateAdhesion())).collect(Collectors.toList());
+            dto.lastAdhesion = a.getLastAdhesion();
+        }).build();
 
-	@Autowired
-	private AdherentRepository adherentRepository;
+    @Autowired
+    private AdherentRepository adherentRepository;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	/**
-	 * Export adherents.
-	 *
-	 * @param format
-	 *            the format (json, xml, csv)
-	 * @param properties
-	 *            the properties to export
-	 * @param adhesionState
-	 *            export adherents with this specified adhesion state
-	 * @param response
-	 *            the http response
-	 * @throws Exception
-	 *             if an error occurs
-	 */
-	public void export(final String format, final List<String> properties, final AdhesionState adhesionState, final HttpServletResponse response) {
-		try {
+    /**
+     * Export adherents.
+     *
+     * @param format        the format (json, xml, csv)
+     * @param properties    the properties to export
+     * @param adhesionState export adherents with this specified adhesion state
+     * @param response      the http response
+     * @throws Exception if an error occurs
+     */
+    public void export(final String format, final List<String> properties, final AdhesionState adhesionState, final HttpServletResponse response) {
+        try {
             final List<AdherentDto> adherents = new ArrayList<>();
 
             Page<Adherent> page = null;
@@ -105,68 +101,68 @@ public class ExportService {
                     exportCsv(adherents, properties, response);
             }
         } catch (IOException | JAXBException ex) {
-		    throw new ExportException("Export failed", ex);
+            throw new ExportException("Export failed", ex);
         }
-	}
+    }
 
-	private AdherentDto mapDto(Adherent adherent, final List<String> properties) {
-		final AdherentDto adherentDto = new AdherentDto();
-		properties.stream().forEach(p -> DTO_MAPPER.get(p).accept(adherent, adherentDto));
-		return adherentDto;
-	}
+    private AdherentDto mapDto(Adherent adherent, final List<String> properties) {
+        final AdherentDto adherentDto = new AdherentDto();
+        properties.stream().forEach(p -> DTO_MAPPER.get(p).accept(adherent, adherentDto));
+        return adherentDto;
+    }
 
-	private boolean filterAdhesionState(Adherent ad, AdhesionState adhesionState) {
-		if (adhesionState == AdhesionState.all) {
-			return true;
-		} else if (adhesionState == AdhesionState.valid) {
-			return ad.getStatutAdhesion() == StatutAdhesion.GREEN;
-		} else if (adhesionState == AdhesionState.expiring) {
-			return ad.getStatutAdhesion() == StatutAdhesion.ORANGE;
-		} else if (adhesionState == AdhesionState.expired) {
-			return ad.getStatutAdhesion() == StatutAdhesion.RED;
-		} else if (adhesionState == AdhesionState.recently_expired) {
-			final LocalDate lastAdhesion = ad.getLastAdhesion();
-			if (lastAdhesion != null) {
-				return ad.getStatutAdhesion() == StatutAdhesion.RED && ad.getLastAdhesion().plusYears(1).isAfter(LocalDate.now().minusMonths(1));
-			} else {
-				return false;
-			}
-		}
-		return true;
-	}
+    private boolean filterAdhesionState(Adherent ad, AdhesionState adhesionState) {
+        if (adhesionState == AdhesionState.all) {
+            return true;
+        } else if (adhesionState == AdhesionState.valid) {
+            return ad.getStatutAdhesion() == StatutAdhesion.GREEN;
+        } else if (adhesionState == AdhesionState.expiring) {
+            return ad.getStatutAdhesion() == StatutAdhesion.ORANGE;
+        } else if (adhesionState == AdhesionState.expired) {
+            return ad.getStatutAdhesion() == StatutAdhesion.RED;
+        } else if (adhesionState == AdhesionState.recently_expired) {
+            final LocalDate lastAdhesion = ad.getLastAdhesion();
+            if (lastAdhesion != null) {
+                return ad.getStatutAdhesion() == StatutAdhesion.RED && ad.getLastAdhesion().plusYears(1).isAfter(LocalDate.now().minusMonths(1));
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	private void exportCsv(List<AdherentDto> dtos, List<String> properties, HttpServletResponse response) throws IOException {
-		response.setHeader(CONTENT_TYPE_HEADER, "text/csv");
+    private void exportCsv(List<AdherentDto> dtos, List<String> properties, HttpServletResponse response) throws IOException {
+        response.setHeader(CONTENT_TYPE_HEADER, "text/csv");
 
-		try (CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(response.getOutputStream()), ';')) {
-			csvWriter.writeNext(new String[] { "ID", "Nom", "Prénom", "Adresse", "Code Postal", "Ville", "Date de dernière adhésion", "Email", "Téléphone" });
+        try (CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(response.getOutputStream()), ';')) {
+            csvWriter.writeNext(new String[]{"ID", "Nom", "Prénom", "Adresse", "Code Postal", "Ville", "Date de dernière adhésion", "Email", "Téléphone"});
 
-			dtos.forEach(a -> csvWriter.writeNext(new String[] { a.id.toString(), a.nom, a.prenom, a.adresse, a.codePostal, a.ville, a.formatLastAdhesion(),
-					a.email, a.telephone }));
-		}
-	}
+            dtos.forEach(a -> csvWriter.writeNext(new String[]{a.id.toString(), a.nom, a.prenom, a.adresse, a.codePostal, a.ville, a.formatLastAdhesion(),
+                a.email, a.telephone}));
+        }
+    }
 
-	private void exportXml(List<AdherentDto> dtos, HttpServletResponse response) throws JAXBException, IOException {
-		response.setHeader(CONTENT_TYPE_HEADER, "text/xml");
+    private void exportXml(List<AdherentDto> dtos, HttpServletResponse response) throws JAXBException, IOException {
+        response.setHeader(CONTENT_TYPE_HEADER, "text/xml");
 
-		final JAXBContext context = JAXBContext.newInstance(Adherents.class);
-		final Marshaller m = context.createMarshaller();
-		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        final JAXBContext context = JAXBContext.newInstance(Adherents.class);
+        final Marshaller m = context.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-		m.marshal(new Adherents(dtos), response.getOutputStream());
-	}
+        m.marshal(new Adherents(dtos), response.getOutputStream());
+    }
 
-	private void exportJson(List<AdherentDto> dtos, HttpServletResponse response) throws IOException {
-		response.setHeader(CONTENT_TYPE_HEADER, "application/json");
-		objectMapper.writeValue(response.getOutputStream(), dtos);
-	}
+    private void exportJson(List<AdherentDto> dtos, HttpServletResponse response) throws IOException {
+        response.setHeader(CONTENT_TYPE_HEADER, "application/json");
+        objectMapper.writeValue(response.getOutputStream(), dtos);
+    }
 
-	@Getter
+    @Getter
     @Setter
-	@JsonInclude(Include.NON_NULL)
-	static class AdherentDto implements Serializable {
+    @JsonInclude(Include.NON_NULL)
+    static class AdherentDto implements Serializable {
 
-		private Long id;
+        private Long id;
         private String nom;
         private String prenom;
         private Boolean estBenevole;
@@ -176,60 +172,60 @@ public class ExportService {
         private String email;
         private String telephone;
         private List<AdhesionDto> adhesions;
-		@JsonIgnore
-		@XmlTransient
+        @JsonIgnore
+        @XmlTransient
         private LocalDate lastAdhesion;
 
-		public String formatLastAdhesion() {
-			if (lastAdhesion != null) {
-				return lastAdhesion.toString("dd/MM/yyyy");
-			}
-			return null;
-		}
+        public String formatLastAdhesion() {
+            if (lastAdhesion != null) {
+                return lastAdhesion.toString("dd/MM/yyyy");
+            }
+            return null;
+        }
 
-	}
+    }
 
-	@Getter
+    @Getter
     @Setter
-	static class AdhesionDto {
+    static class AdhesionDto {
 
-		private TypeAdhesion typeAdhesion;
+        private TypeAdhesion typeAdhesion;
 
-		@JsonSerialize(using = CustomLocalDateSerializer.class)
-		@XmlJavaTypeAdapter(LocalDateAdapter.class)
-		private LocalDate dateAdhesion;
+        @JsonSerialize(using = CustomLocalDateSerializer.class)
+        @XmlJavaTypeAdapter(LocalDateAdapter.class)
+        private LocalDate dateAdhesion;
 
-		public AdhesionDto(TypeAdhesion typeAdhesion, LocalDate dateAdhesion) {
-			super();
-			this.typeAdhesion = typeAdhesion;
-			this.dateAdhesion = dateAdhesion;
-		}
+        public AdhesionDto(TypeAdhesion typeAdhesion, LocalDate dateAdhesion) {
+            super();
+            this.typeAdhesion = typeAdhesion;
+            this.dateAdhesion = dateAdhesion;
+        }
 
-	}
+    }
 
-	@XmlRootElement
-	static class Adherents implements Serializable {
+    @XmlRootElement
+    static class Adherents implements Serializable {
 
-		private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-		private List<AdherentDto> adherent;
+        private List<AdherentDto> adherent;
 
-		public Adherents() {
-		}
+        public Adherents() {
+        }
 
-		public Adherents(List<AdherentDto> adherent) {
-			super();
-			this.adherent = adherent;
-		}
+        public Adherents(List<AdherentDto> adherent) {
+            super();
+            this.adherent = adherent;
+        }
 
-		public List<AdherentDto> getAdherent() {
-			return adherent;
-		}
+        public List<AdherentDto> getAdherent() {
+            return adherent;
+        }
 
-		public void setAdherent(List<AdherentDto> adherent) {
-			this.adherent = adherent;
-		}
+        public void setAdherent(List<AdherentDto> adherent) {
+            this.adherent = adherent;
+        }
 
-	}
+    }
 
 }
