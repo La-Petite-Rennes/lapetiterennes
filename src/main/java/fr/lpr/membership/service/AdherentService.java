f89fadfd77b8a0138518789c772cc62e7b64a946
Goulven Le Breton <goulven.lebreton+github@gmail.com>
2019-04-26 15:03:13 +0200

    Envoi de mail pour les adhérents dont la date d'adhésion vient d'expirer

--- src/main/java/fr/lpr/membership/service/AdherentService.java
+++ src/main/java/fr/lpr/membership/service/AdherentService.java
@@ -8,7 +8,6 @@ import fr.lpr.membership.repository.AdherentRepository;
 import fr.lpr.membership.web.rest.util.PaginationUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.joda.time.LocalDate;
 import org.springframework.data.domain.Page;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -18,6 +17,8 @@ import javax.mail.MessagingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.joda.time.LocalDate.now;
 
 @Service
 @Slf4j
@@ -43,7 +44,7 @@ public class AdherentService {
                 .filter(ad -> ad.getStatutAdhesion() == StatutAdhesion.ORANGE)
                 .filter(ad -> ad.lastAdhesion().map(adhesion -> adhesion.getTypeAdhesion() != TypeAdhesion.Mensuelle).orElse(false))
 				.filter(ad -> !Strings.isNullOrEmpty(ad.getCoordonnees().getEmail()))
-				.filter(ad -> ad.getReminderEmail() == null || ad.getReminderEmail().isBefore(LocalDate.now().minusMonths(1)))
+				.filter(ad -> ad.getReminderEmail() == null || ad.getReminderEmail().isBefore(now().minusMonths(1)))
 				.collect(Collectors.toList()));
 		} while (page.hasNext());
 
@@ -59,10 +60,40 @@ public class AdherentService {
 		log.info("{} emails de rappel de cotisation envoyés", adherents.size());
 	}
 
+    @Scheduled(cron = "0 0 9 * * *", zone = "GMT")
+    public void adhesionExpired() {
+        log.info("Démarrage du batch d'envoi d'email des adhérents dont l'adhésion vient d'expirer");
+
+        // Get Adherents
+        final List<Adherent> adherents = new ArrayList<>();
+
+        Page<Adherent> page = null;
+        do {
+            page = adherentRepository.findAll(page == null ? PaginationUtil.generatePageRequest(0, 100) : page.nextPageable());
+
+            adherents.addAll(page.getContent().stream()
+                .filter(ad -> ad.getStatutAdhesion() == StatutAdhesion.RED)
+				.filter(ad -> !Strings.isNullOrEmpty(ad.getCoordonnees().getEmail()))
+				.filter(ad -> ad.lastAdhesion().map(adh -> adh.getDateFinAdhesion().isAfter(now().minusDays(1))).orElse(false))
+				.collect(Collectors.toList()));
+        } while (page.hasNext());
+
+        // Send mail for each adherents
+        adherents.forEach(ad -> {
+            try {
+                mailService.sendAdhesionExpiredEmail(ad);
+            } catch (final Exception ex) {
+                log.error("E-mail could not be sent to adherent '{}', exception is: {}", ad.getPrenom() + " " + ad.getNom(), ex.getMessage());
+            }
+        });
+
+        log.info("{} emails de fin d'adhésion envoyés", adherents.size());
+    }
+
 	@Transactional
 	public void sendReminderMail(Adherent adherent) throws MessagingException {
 		mailService.sendAdhesionExpiringEmail(adherent);
-		adherent.setReminderEmail(LocalDate.now());
+		adherent.setReminderEmail(now());
 		adherentRepository.save(adherent);
 	}
 
