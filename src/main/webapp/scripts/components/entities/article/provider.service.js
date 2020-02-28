b5fc234c973cbcbf970a15ed2b69326de0891ea1
Goulven Le Breton <goulven.lebreton+github@gmail.com>
2020-02-28 16:07:50 +0100

    Delete AngularJS application

--- src/main/webapp/scripts/components/entities/article/provider.service.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('membershipApp')
-    .factory('Provider', function ($resource) {
-        return $resource('api/providers/:id', {}, {});
-    });
--- src/main/webapp/scripts/components/form/pagination.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('membershipApp')
-    .directive('membershipAppPagination', function() {
-        return {
-            templateUrl: 'scripts/components/form/pagination.html'
-        };
-    });
